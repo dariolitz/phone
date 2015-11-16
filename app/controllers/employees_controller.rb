3b1bdd97e2b1078493988243aa68b0a1a822a805
dariolitz <d.litz@lifecodexx.com>
2015-11-16 16:21:46 +0100

    employees table

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,11 @@
+class EmployeesController < ApplicationController
+  def index
+  	@employees = Employee.all
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create( name: 'Chicago' ,  name: 'Copenhagen' )
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+department = Department.create(name: "IT")
+department2 = Department.create(name: "Vertrieb")
+employee = Employee.create(firstname: "hans", name: "peter", phone: "0123125215", email: "hans.peter@email.com", location: "germany", function: "nil", department_id: department.id)
+employee2 = Employee.create(firstname: "christoph", name: "werner", phone: "012321214", email: "christoph.werner@email.com", location: "austria", function: "nil", department_id: department2.id)
